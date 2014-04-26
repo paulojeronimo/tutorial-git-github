93a348b9d3143949cf3fc22b389df596f2ce69ae
Paulo Jerônimo <pj@paulojeronimo.info>
2014-04-26 12:54:20 -0300

    desenvolvendo

--- slides-no-github/ajustar-usuario.sh
+++ slides-no-github/ajustar-usuario.sh
@@ -3,13 +3,14 @@
 source "$TUTO_HOME/ambiente"
 
 case `uname -a` in
-   Darwin) sed_i='sed -i \"\"';;
-   Linux) sed_i='sed -i';;
+   Darwin) sed_i=sed -i \"\";;
+   Linux) sed_i=sed -i;;
 esac
 
 for f in *.asciidoc
 do 
    $sed_i "s/SEU_NOME/$SEU_NOME/g
+   s/SEU_EMAIL/$SEU_EMAIL/g
    s/SEU_USUARIO/$SEU_USUARIO/g
    s/SEU_TWITTER/$SEU_TWITTER/g" $f
 done
